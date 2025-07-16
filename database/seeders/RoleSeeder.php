25254f722652e0fab2de9d1d4252dcbd08d9467c
AJytc <yutucaj07@gmail.com>
2025-07-16 22:40:57 +0800

    paldo

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -14,6 +14,7 @@ class RoleSeeder extends Seeder
     public function run(): void
     {
         Role::create(['name' => 'admin']);
+        Role::create(['name' => 'medstaff']);
         Role::create(['name' => 'user']);
     }
 }
